555b8d23ec47bd100cbb2c5ce3dc7d70cb8b0741
Aryan Saini <155607558+aryansainias@users.noreply.github.com>
2024-07-06 13:06:18 +0530

    Upload user.js

---
+++ src/data/user.js
@@ -0,0 +1,109 @@
+// data/user.js
+
+
+const INFO = {
+	main: {
+	  title: "My Portfolio App",
+	  name: "Aryan Saini",
+	  email: "mail@aryansainias2000@gmail.com",
+	  logo: "../logo.png",
+	},
+  
+	socials: {
+	  twitter: "https://twitter.com/",
+	  github: "https://github.com/aryansainias",
+	  linkedin: "https://linkedin.com/in/aryan-saini-016339222",
+	  instagram: "https://instagram.com/",
+	  stackoverflow: "https://stackoverflow.com/",
+	  facebook: "https://facebook.com/",
+	},
+  
+	homepage: {
+	  title: "Software Dev.",
+	  description:
+		"I am currently an student pursuing Bachelor of Technology in Computer Science and Engineering, passionate about software development. My experience includes building web applications that prioritize scalability, security, and reliability using various frameworks and technologies. I enjoy tackling complex problems and continually expanding my skill set. Committed to producing high-quality code that aligns with best practices and industry standards, I am eagerly seeking new challenges and opportunities for personal and professional growth in the field.",
+	},
+	about: {
+	  title: "Hi , I’m Aryan. I live in India",
+	  description:
+		"I'm a software development enthusiast focusing on languages like Java, JavaScript, and Python. Currently studying, I love creating practical, user-friendly applications. I enjoy exploring different frameworks and methods to improve my software design skills. I'm eager to bring my creativity and problem-solving abilities to exciting projects, always learning and adapting to new technologies in this fast-paced field.",
+	},
+  
+	projects: [
+	  {
+		title: "YummyWheelz-Pizza-App",
+		description:
+		  "Yummy Wheelz Pizza Delivery simplifies the process of ordering pizza online. With our app, you can browse through a variety of pizzas, customize your order, and have it delivered quickly. Whether you're craving classic pepperoni or adventurous with exotic toppings, we've got you covered.",
+		logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+		linkText: "View Project",
+		link: "https://github.com/aryansainias/YummyWheelz-Pizza-App.git",
+	  },
+	  
+	],
+  
+	certificates: [
+	  {
+		title: " Data Structures Certification" ,logo: "certificate.png",
+		description: " an online non-credit course authorized by University of California San Diego.",
+		date: "Mar 2023",
+		
+		linkText:"Verify",
+		verifyLink: "https://www.coursera.org/account/accomplishments/verify/G4DJL34ZXG7Q",
+	  },
+
+	  
+	  {
+		title: "Develop solutions with Azure AI Document Intelligence", logo: "certificate.png",
+		description: "I had the opportunity to participate in the Microsoft AI Skills Challenge, a comprehensive and engaging online training program that equipped me with the fundamentals of AI development. Through this program, I gained hands-on experience with the latest tools and technologies, enabling me to design and build intelligent and scalable applications.",
+		date: "Jan 2024",
+		linkText:"Verify",
+		verifyLink: "https://learn.microsoft.com/api/achievements/share/en-gb/AryanSaini-7966/EJNZTWXP?sharingId=E3D9CDA7F7A004E5",
+	  },
+	  
+	  {
+		title: "Microsoft Azure Data Fundamentals: Explore core data concepts", logo: "certificate.png",
+		description: "Data is the foundation on which all software is built.I learned about common data formats, workloads, roles, and services.This learning path helped me prepare for the Azure Data Fundamentals certification.",
+		date: "Sept 2022",
+		linkText:"Verify",
+		verifyLink: "https://learn.microsoft.com/api/achievements/share/en-gb/AryanSaini-7966/ZMFHNS62?sharingId=E3D9CDA7F7A004E5",
+	  },
+
+	   
+	  {
+		title: "Object Oriented Programming in Java", logo: "certificate.png",
+		description: " an online non-credit course authorized by University of California San Diego and offered through Coursera",
+		date: "Nov 2022",
+		linkText:"Verify",
+		verifyLink: "https://www.coursera.org/account/accomplishments/verify/8PYUYUFJY5RK",
+	  },
+
+	  {
+		title: " Java Programming: Arrays, Lists, and Structured Data", logo: "certificate.png",
+		description: "  an online non-credit course authorized by Duke University and offered through Coursera",
+		date: "Nov 2022",
+		linkText:"Verify",
+		verifyLink: "https://www.coursera.org/account/accomplishments/verify/TD73D6U35DQQ",
+	  },
+
+	  {
+		title: " Elastic Google Cloud Infrastructure: Scaling and Automation", logo: "certificate.png",
+		description: " an online non-credit course authorized by Google Cloud .",
+		date: "Mar 2024",
+		linkText:"Verify",
+		verifyLink: "https://www.coursera.org/account/accomplishments/verify/LBHK39HFKC4B",
+	  },
+
+	  {
+		title: "Develop natural language processing solutions with Azure AI Services", logo: "certificate.png",
+		description: "Natural language processing (NLP) solutions use language models to interpret the semantic meaning of written or spoken language.Used the Language Understanding service to build language models for applications.",
+		date: "Jan 2024",
+		linkText:"Verify",
+		verifyLink: "https://learn.microsoft.com/api/achievements/share/en-gb/AryanSaini-7966/YVG8ZUQR?sharingId=E3D9CDA7F7A004E5",
+	  },
+
+	
+	],
+  };
+  
+  export default INFO;
+  
