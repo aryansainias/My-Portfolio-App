99ad344053074a8352cc67fb83625ef3adbb9a1a
Aryan Saini <155607558+aryansainias@users.noreply.github.com>
2024-07-06 13:06:44 +0530

    Add files via upload

---
+++ src/data/seo.js
@@ -0,0 +1,39 @@
+const SEO = [
+	{
+		page: "home",
+		description:
+			"I am currently an student pursuing Bachelor of Technology in Computer Science and Engineering, passionate about software development. My experience includes building web applications that prioritize scalability, security, and reliability using various frameworks and technologies. I enjoy tackling complex problems and continually expanding my skill set. Committed to producing high-quality code that aligns with best practices and industry standards, I am eagerly seeking new challenges and opportunities for personal and professional growth in the field.",
+		keywords: ["Aryan", "Aryan Saini"],
+	},
+
+	{
+		page: "about",
+		description:
+			"I'm a software development enthusiast focusing on languages like Java, JavaScript, and Python. Currently studying, I love creating practical, user-friendly applications. I enjoy exploring different frameworks and methods to improve my software design skills. I'm eager to bring my creativity and problem-solving abilities to exciting projects, always learning and adapting to new technologies in this fast-paced field.",
+		keywords: ["Aryan", "Aryan Saini"],
+	},
+
+
+	{
+		page: "projects",
+		description:
+			"I've worked on a variety of projects and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to.",
+		keywords: ["Aryan", "Aryan Saini"],
+	},
+
+	{
+		page: "certificates",
+		description:
+			"This section showcases my professional certifications, awards, and other relevant credentials. I've chosen to highlight these achievements to demonstrate my commitment to continuous learning and my ability to adapt to new technologies and industry trends.",
+		keywords: ["Aryan", "Aryan Saini"],
+	},
+
+	{
+		page: "contact",
+		description:
+			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
+		keywords: ["Aryan", "Aryan Saini",],
+	},
+];
+
+export default SEO;
